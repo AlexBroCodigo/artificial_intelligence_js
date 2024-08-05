80e17e94a576694653447b1f4120f264f1f38aeb
Alex Chinchilla <65095924+AlexBroCodigo@users.noreply.github.com>
2024-08-04 20:21:18 -0600

    Update 01_reflex_agent.js

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,25 +1,70 @@
-// MIT License
-// Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
+function reflex_agent(location, state) {
+    if (state === "DIRTY") {
+        return 'CLEAN';
+    } else if (location === 'A') {
+        return 'RIGHT';
+    } else if (location === 'B') {
+        return 'LEFT';
+    }
 }
 
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";		
-	setTimeout(function(){ test(states); }, 2000);
+function reflex_agent_D(location, state) {
+    if (state === "CLEAN") {
+        return 'DIRTY';
+    } else if (location === 'A') {
+        return 'RIGHT';
+    } else if (location === 'B') {
+        return 'LEFT';
+    }
 }
 
-var states = ["A","DIRTY","DIRTY"];
-test(states);
+function test(states) {
+    let ensuciar = false;
+    const interval = setInterval(() => {
+        let location = states[0];
+        let state = (location === 'A') ? states[1] : states[2];
+        
+        if (ensuciar) {
+            let action = reflex_agent_D(location, state);
+	    document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action);
+            
+            if (action === "DIRTY") {
+                if (location === 'A') {
+                    states[1] = "DIRTY";
+                } else if (location === 'B') {
+                    states[2] = "DIRTY";
+                }
+            } else if (action === "RIGHT") {
+                states[0] = 'B'; // Aquí de A me vengo a B
+            } else if (action === "LEFT") {
+                states[0] = 'A'; // Aquí de B me vengo a A
+            }
+            
+            // Activar banderita de que ya se limpió todo
+	    document.getElementById("log").innerHTML+="<br>Nos iremos a limpiar";
+            ensuciar = false;
+        } else {
+            let action = reflex_agent(location, state);
+            document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action);
+            
+            if (action === "CLEAN") {
+                if (location === 'A') {
+                    states[1] = "CLEAN";
+                } else if (location === 'B') {
+                    states[2] = "CLEAN";
+                }
+            } else if (action === "RIGHT") {
+                states[0] = 'B'; // Aquí de A me vengo a B
+            } else if (action === "LEFT") {
+                states[0] = 'A'; // Aquí de B me vengo a A
+            }
+            
+            // Activar banderita de que ya se limpió todo
+            document.getElementById("log").innerHTML+="<br>Nos iremos a ensuciar";
+            ensuciar = true;
+        }
+    }, 3000);
+}
+
+test(['A', 'DIRTY', 'DIRTY']);
